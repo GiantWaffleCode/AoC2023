b85ca3c2d617dea6587be8efa51b0db1f5287e2f
GiantWaffleCode <72772325+GiantWaffleCode@users.noreply.github.com>
2023-12-14 10:54:31 -0600

    final day 14

--- Day14/py_day14_part2.py
+++ Day14/py_day14_part2.py
@@ -1,8 +1,11 @@
 #   Advent of Code 2023
 #      GiantWaffle
 #   Challenge 14 of 25
-#      Part 1 of 2
+#      Part 2 of 2
 # -----------------------
+
+from tqdm import tqdm
+
 
 use_real_data = True
 
@@ -38,6 +41,14 @@ starting_map = [] #starting map of rocks
 for line in test_input:
     starting_map.append(line)
 
+def rotate_matrix(matrix):
+    rm = list(zip(*matrix[::-1]))
+    new_matrix = []
+    for line in rm:
+        line_str = ''.join(line)
+        new_matrix.append(line_str)
+    return new_matrix
+
 
 def shift_up_once(map):
     s = map.copy()
@@ -48,13 +59,11 @@ def shift_up_once(map):
                 s[i+1] = s[i+1][:index] + '.' + s[i+1][index+1:]
     return s
 
-
 def tilt_north(map):
     p = map.copy()
     for i in range(len(p)-1):
         p = shift_up_once(p)
     return p
-
 
 def calc_north_load(map):
     total_lines = len(map)
@@ -65,8 +74,35 @@ def calc_north_load(map):
     return load
 
 
-tilted_map = tilt_north(starting_map)
-north_load = calc_north_load(tilted_map)
+# cycles = 1000000000
+cycles = 1000
 
-print(f'Part 1 Answer: {north_load}')
 
+
+spin_map = starting_map.copy()
+
+load_history = []
+pattern = [90788, 90795, 90785, 90787, 90781, 90790, 90784, 90796, 90794, 90795, 90788, 90792, 90780, 90788, 90783, 90791, 90789, 90801]
+pattern_cycle_start = 0
+
+for count in tqdm(range(cycles)):
+    for r in range(4):
+        spin_map = tilt_north(spin_map)
+        spin_map = rotate_matrix(spin_map)
+    north_load = calc_north_load(spin_map)
+    if len(load_history) > len(pattern)-1 :
+        load_history.pop(0)
+    load_history.append(north_load)
+    if load_history == pattern:
+        pattern_cycle_start = count
+        break
+
+print(load_history)
+print(pattern_cycle_start)
+print(f'Part 2 Answer: {load_history[0]}')
+
+
+#very very very hacky solution
+rem = (1000000000-144) % len(pattern)
+print(rem)
+print(pattern[rem])
